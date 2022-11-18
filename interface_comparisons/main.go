551d52c9b31ba88b1cf253912e024d84ef6696f7
Herbie <hjrand1005@gmail.com>
2022-11-18 13:35:54 -0500

    ADD: nil interface comparisons example

--- interface_comparisons/main.go
+++ interface_comparisons/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-  "fmt"
+	"bytes"
+	"fmt"
+	"io"
 )
 
-func main() {
-  var x interface{} = 1
-  fmt.Println(x == x) // true!
+// interfaceComparisons illustrates that not all interface values are comparable.
+func interfaceComparisons() {
+	var x interface{} = 1
+	fmt.Println(x == x) // true!
 
-  var y interface{} = []int{1, 2, 3}
-  fmt.Println(y == y) // panics
+	var y interface{} = []int{1, 2, 3}
+	fmt.Println(y == y) // panics
+}
+
+// nilComparisons illustrates that a nil interface is different from an interface
+// containing a nil value.
+func nilComparisons() {
+	var buf *bytes.Buffer
+	fmt.Println(writerIsNil(buf)) // false!
+
+	var w io.Writer
+	fmt.Println(writerIsNil(w)) // true!
+}
+
+func writerIsNil(w io.Writer) bool {
+	return w == nil
+}
+
+func main() {
+	// interfaceComparisons()
+	nilComparisons()
 }
