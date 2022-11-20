b917e42f0a88619be1a5a986859ddaf88d30e394
Herbie <hjrand1005@gmail.com>
2022-11-19 23:39:42 -0500

    FIX: make generic

--- algorithms/quicksort/quicksort.go
+++ algorithms/quicksort/quicksort.go
@@ -1,30 +1,35 @@
 package main
 
-// quicksort can sort a slice of integers
-func quicksort(s []int, low int, high int) {
+type number interface {
+	uint8 | uint16 | uint32 | uint64 |
+		int | int8 | int16 | int32 | int64
+}
+
+// quicksort can sort a slice of numbers
+func quicksort[T number](slice []T, low, high T) {
 	if low >= high {
 		return
 	}
 
-	p := partition(s, low, high)
-	quicksort(s, low, p-1)
-	quicksort(s, p+1, high)
+	p := partition(slice, low, high)
+	quicksort(slice, low, p-1)
+	quicksort(slice, p+1, high)
 }
 
 // partition is responsible for ensuring some pivot index is
 // put in its final position. It returns that index
-func partition(s []int, low int, high int) int {
-	pivot := s[high]
+func partition[T number](slice []T, low, high T) T {
+	pivot := slice[high]
 	i := low
 
 	for j := low; j < high; j++ {
-		if s[j] < pivot {
-			s[i], s[j] = s[j], s[i]
+		if slice[j] < pivot {
+			slice[j], slice[i] = slice[i], slice[j]
 			i++
 		}
 	}
 
-	s[i], s[high] = s[high], s[i]
+	slice[high], slice[i] = slice[i], slice[high]
 
 	return i
 }
