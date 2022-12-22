0b4d0609ec467e735a92432b8a9540faa00b9c95
Herbie Rand <hjrand1005@gmail.com>
2022-12-22 17:19:57 -0500

    FIX: go fmt

--- go-book/ch8/reverb/main.go
+++ go-book/ch8/reverb/main.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "net"
-    "strings"
-    "time"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
 )
 
 func main() {
-    listener, err := net.Listen("tcp", "localhost:8000")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            log.Print(err)
-            continue
-        }
-        handleConn(conn)
-    }
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)
+	}
 }
 
 // no goroutine for reverb1
 func handleConn(c net.Conn) {
-    input := bufio.NewScanner(c)
-    for input.Scan() {
-        echo(c, input.Text(), 1*time.Second)
-    }
-    if err := input.Err(); err != nil {
-        log.Print(err)
-    }
-    c.Close()
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	if err := input.Err(); err != nil {
+		log.Print(err)
+	}
+	c.Close()
 }
 
 func echo(c net.Conn, shout string, delay time.Duration) {
-    fmt.Fprintln(c, "\t", strings.ToUpper(shout))
-    time.Sleep(delay)
-    fmt.Fprintln(c, "\t", shout)
-    time.Sleep(delay)
-    fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
-
