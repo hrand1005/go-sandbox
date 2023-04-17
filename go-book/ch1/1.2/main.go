d93fb2c33114c622f771feb31f1c15ca13370eba
Herbie <hjrand1005@gmail.com>
2023-04-17 10:13:30 -0400

    FIX: 1.2 simple print

--- go-book/ch1/1.2/main.go
+++ go-book/ch1/1.2/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"os"
 )
 
 func main() {
-	var report strings.Builder
 	for i := 1; i < len(os.Args); i++ {
-		report.WriteString(fmt.Sprintf("%d: %v", i, os.Args[i]))
-		if i != len(os.Args)-1 {
-			report.WriteString("\n")
-		}
+		fmt.Printf("%d: %v\n", i, os.Args[i])
 	}
-	fmt.Println(report.String())
 }
