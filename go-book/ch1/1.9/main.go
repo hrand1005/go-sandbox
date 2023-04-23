30ea71902c2c19ea293aac6a916be92d2f3eaff6
Herbie <hjrand1005@gmail.com>
2023-04-23 15:49:33 -0400

    ADD: 1.7, 1.8, 1.9 exercises

---
+++ go-book/ch1/1.9/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Received HTTP Response with status code: %v\n", resp.Status)
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
